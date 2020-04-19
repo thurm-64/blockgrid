5aac15a706ba43d9a852062c38b4cfa079170fa7
thurm-64 <63815877+thurm-64@users.noreply.github.com>
2020-04-19 11:14:21 -0400

    Create blocks.js

---
+++ blocks.js
@@ -0,0 +1,80 @@
+const block = {
+
+	air: {
+	
+			name: "Air",
+			texture: "data:image/gif;base64,R0lGODlhAQABAPAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw=="
+		 
+	},  
+	rubyore: {
+	
+			name: "Ruby Ore",
+			texture: "textures/rubyore.png"
+	}, 
+	stone: {
+
+		name: "Stone",
+		texture: "textures/stone.png"
+	 
+	},
+	grass: {
+
+		name: "Grass",
+		texture: "textures/grass.png"
+	 
+	},
+	log: {
+
+		name: "Log",
+		texture: "textures/log.png"
+	 
+	},
+	dirt: {
+
+		name: "Dirt",
+		texture: "textures/dirt.png"
+	 
+	},
+	leaves: {
+
+		name: "Leaves",
+		texture: "textures/leaves.png"
+	 
+	},
+	gravel: {
+
+		name: "Gravel",
+		texture: "textures/gravel.png"
+	 
+	},
+	cactus: {
+
+		name: "Gravel",
+		texture: "textures/cactus.png"
+	 
+	},
+	goldore: {
+
+		name: "Gold Ore",
+		texture: "textures/goldore.png"
+	 
+	},
+	sand: {
+
+		name: "Sand",
+		texture: "textures/sand.png"
+	 
+	},
+	sandstone: {
+
+		name: "Sandstone",
+		texture: "textures/sandstone.png"
+	 
+	},
+	diamondore: {
+
+		name: "Diamond Ore",
+		texture: "textures/diamondore.png"
+	 
+	},
+};
